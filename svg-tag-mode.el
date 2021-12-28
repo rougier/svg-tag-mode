202a70dc25625b1b9d5a86e916095b41f5bb0e4e
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2021-12-28 19:41:34 +0100

    Changed documentation for lambda expressions only.

--- svg-tag-mode.el
+++ svg-tag-mode.el
@@ -33,7 +33,8 @@
 ;; Usage example:
 ;; --------------
 ;;
-;; (setq svg-tag-tags '((":TODO:"  ((svg-tag-make "TODO") nil nil))))
+;; (setq svg-tag-tags '((":TODO:"  ((lambda (tag)
+;;                                      (svg-tag-make "TODO"))))))
 ;;
 ;; Each item has the form '(KEYWORD (TAG COMMAND HELP)) where:
 ;;  - KEYWORD is a regular expression including a matched group of 
@@ -51,55 +52,49 @@
 ;; Examples:
 ;; ---------
 ;;
-;; ;; This replaces any occurence of ":TODO:" with a static SVG tag
+;; ;; This replaces any occurence of ":TODO:" with a SVG tag
 ;; ;; displaying "TODO"
 ;; (setq svg-tag-tags
-;;       '((":TODO:" . ((svg-tag-make "TODO")))))
+;;       '((":TODO:" . ((lambda (tag) (svg-tag-make "TODO" ))))))
 ;;
-;; ;; This replaces any occurence of ":HELLO:" with a static SVG tag that
+;; ;; This replaces any occurence of ":HELLO:" with a SVG tag that
 ;; ;; can be clicked to execute the specified command. Help message is
 ;; ;; displayed when the tag is hovered with the pointer.
 ;; (setq svg-tag-tags
-;;       '((":HELLO:" .  ((svg-tag-make "HELLO")
+;;       '((":HELLO:" .  ((lambda (tag) (svg-tag-make "HELLO"))
 ;;                        (lambda () (interactive) (message "Hello world!"))
 ;;                        "Print a greeting message"))))
 ;;
-;; ;; This replaces any occurence of ":TODO:" with a dynamic SVG tag
-;; ;; displaying ":TODO:"
-;; (setq svg-tag-tags
-;;       '((":TODO:" . (svg-tag-make))))
-;;
-;; ;; This replaces any occurence of ":TODO:" with a dynamic SVG tag
-;; ;; displaying "TODO"
-;; (setq svg-tag-tags
-;;       '((":TODO:" . ((lambda (tag)
-;;                        (svg-tag-make tag :beg 1 :end -1))))))
-;;
-;; ;; This replaces any occurence of ":XXX:" with a dynamic SVG tag
-;; ;; displaying "XXX"
+;; ;; This replaces any occurence of ":XYZ:" with a SVG tag
+;; ;; displaying "XYZ"
 ;; (setq svg-tag-tags
 ;;       '(("\\(:[A-Z]+:\\)" . ((lambda (tag)
-;;                                (svg-tag-make tag :beg 1 :end -1))))))
+;;                                  (svg-tag-make tag :beg 1 :end -1))))))
 ;;
 ;; ;; This replaces any occurence of ":XXX|YYY:" with two adjacent
-;; ;; dynamic SVG tags displaying "XXX" and "YYY"
+;; ;; SVG tags displaying "XXX" and "YYY"
 ;; (setq svg-tag-tags
-;;       '(("\\(:[A-Z]+\\)\|[a-zA-Z#0-9]+:" . ((lambda (tag)
-;;                                            (svg-tag-make tag :beg 1 :inverse t
-;;                                                           :margin 0 :crop-right t))))
-;;         (":[A-Z]+\\(\|[a-zA-Z#0-9]+:\\)" . ((lambda (tag)
-;;                                            (svg-tag-make tag :beg 1 :end -1
-;;                                                          :margin 0 :crop-left t))))))
+;;       '(("\\(:[A-Z]+\\)\|[a-zA-Z#0-9]+:" .
+;;                  ((lambda (tag) (svg-tag-make tag :beg 1
+;;                                                   :inverse t
+;;                                                   :margin 0
+;;                                                   :crop-right t))))
+;;         (":[A-Z]+\\(\|[a-zA-Z#0-9]+:\\)" .
+;;                  ((lambda (tag) (svg-tag-make tag :beg 1
+;;                                                   :end -1
+;;                                                   :margin 0
+;;                                                   :crop-left t))))))
 ;;
 ;; ;; This replaces any occurence of ":#TAG1:#TAG2:…:$" ($ means end of
-;; ;; line) with a dynamic collection of SVG tags. Note the # symbol in
+;; ;; line) with a collection of SVG tags. Note the # symbol in
 ;; ;; front of tags. This is mandatory because Emacs cannot do regex look
 ;; ;; ahead.
 ;; (setq svg-tag-tags
 ;;       '(("\\(:#[A-Za-z0-9]+\\)" . ((lambda (tag)
 ;;                                      (svg-tag-make tag :beg 2))))
 ;;         ("\\(:#[A-Za-z0-9]+:\\)$" . ((lambda (tag)
-;;                                        (svg-tag-make tag :beg 2 :end -1))))))
+;;                                        (svg-tag-make tag :beg 2
+;;                                                          :end -1))))))
 ;;
 ;;; NEWS:
 ;;
