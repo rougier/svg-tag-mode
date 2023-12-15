4b163bd10f2b43af523dc2f18a531d7be98860d4
Jonas Bernoulli <jonas@bernoul.li>
2023-12-15 20:34:25 +0100

    Fix whitespace errors

--- svg-tag-mode.el
+++ svg-tag-mode.el
@@ -36,7 +36,7 @@
 ;;                                      (svg-tag-make "TODO"))))))
 ;;
 ;; Each item has the form '(KEYWORD (TAG COMMAND HELP)) where:
-;;  - KEYWORD is a regular expression including a matched group of 
+;;  - KEYWORD is a regular expression including a matched group of
 ;;    the form "\\(xxx\\)". If this is not the case the whole
 ;;    string will be used a the matched group.
 ;;  - TAG is either a SVG image that will be displayed using the
@@ -134,7 +134,7 @@ This is in contrast to merely setting it to 0."
   (let (p)
     (while plist
       (if (not (eq property (car plist)))
-	  (setq p (plist-put p (car plist) (nth 1 plist))))
+          (setq p (plist-put p (car plist) (nth 1 plist))))
       (setq plist (cddr plist)))
     p))
 
@@ -172,7 +172,7 @@ attribute from ``svg-tag-default-face''."
 
 (defun svg-tag-make (tag &optional &rest args)
   "Return a svg tag displaying TAG and using specified ARGS.
-   
+
   ARGS are passed to the `svg-lib-tag' function but there are
   supplementary arguments:
 
@@ -182,7 +182,7 @@ attribute from ``svg-tag-default-face''."
   :end (integer) specifies the last index of the tag substring to
                  take into account (default nil)
 
-  :face (face) indicates the face, property list or string to use to 
+  :face (face) indicates the face, property list or string to use to
                compute foreground & background color. (default `default')
 
   :inverse (bool) indicates whether to inverse foreground &
@@ -192,7 +192,7 @@ attribute from ``svg-tag-default-face''."
    cannot be specified because thay are overwritten by the
    function. If you need full control of tag appearance, best is
    to call svg-lib-tag directly."
-  
+
   (let* ((face (or (plist-get args :face) 'svg-tag-default-face))
          (foreground (svg-tag--face-attribute face :foreground))
          (background (svg-tag--face-attribute face :background))
@@ -235,7 +235,7 @@ attribute from ``svg-tag-default-face''."
             (font-lock-flush beg end )
           (if (and (not view-read-only) (not buffer-read-only))
               (font-lock-unfontify-region beg end))))
-    
+
     (if (eq svg-tag-action-at-point 'echo)
         (if (eq direction 'entered)
             (let ((message-log-max nil))
@@ -246,7 +246,7 @@ attribute from ``svg-tag-default-face''."
 
 (defun svg-tag--build-keywords (item)
   "Process an item in order to install it as a new keyword."
-    
+
   (let* ((pattern  (if (string-match "\\\\(.+\\\\)" (car item))
                        (car item)
                      (format "\\(%s\\)" (car item))))
@@ -279,7 +279,7 @@ attribute from ``svg-tag-default-face''."
         (apply oldfun args))
     (advice-remove 'remove-text-properties
                      #'svg-tag--remove-text-properties)))
-    
+
 (defun svg-tag-mode-on ()
   "Activate SVG tag mode."
   (add-to-list 'font-lock-extra-managed-props 'display)
@@ -306,7 +306,7 @@ attribute from ``svg-tag-default-face''."
   ;; Flush buffer when entering read-only
   (add-hook 'read-only-mode-hook
             #'(lambda () (font-lock-flush (point-min) (point-max))))
-  
+
   ;; Redisplay everything to show tags
   (cursor-sensor-mode 1)
   (font-lock-flush))
